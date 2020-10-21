1eb001c113763fd08bfaf5bab84e5b96f49906f4
Andrew <andrei_efremov@inbox.ru>
2020-10-22 00:47:10 +0400

    made accommodatio filer

--- js/pins.js
+++ js/pins.js
@@ -2,18 +2,59 @@
 (function () {
   let pinTemplate = document.querySelector("#pin").content.querySelector(".map__pin");
   let mapPins = document.querySelector(".map__pins");
-  const pinsLength = 7;
+  let housingType = document.querySelector('#housing-type');
+  let serverData = [];
+  const MAX_PINS_ON_MAP = 5;
 
-  window.successHandler = function (serverData) {
-    for (let i = 0; i <= pinsLength; i++) {
+  let removePins = function () {
+    let drawnPins = document.querySelectorAll('.map__pin');
+    for (let i = drawnPins.length - 1; i >= 1; i--) {
+      mapPins.removeChild(drawnPins[i]);
+    }
+  };
+
+  let sliceServerData = function (data) {
+    if (data.length > MAX_PINS_ON_MAP) {
+      return data.slice(0, MAX_PINS_ON_MAP);
+    } else {
+      return data;
+    }
+  };
+
+  let drawPins = function (data) {
+    let slicedData = sliceServerData(data);
+    for (let i = 0; i < slicedData.length; i++) {
       let pinElement = pinTemplate.cloneNode(true);
-      pinElement.querySelector("img").src = serverData[i].author.avatar;
-      pinElement.querySelector("img").alt = serverData[i].author.title;
-      pinElement.style.left = serverData[i].location.x + "px";
-      pinElement.style.top = serverData[i].location.y + "px";
+      pinElement.querySelector("img").src = slicedData[i].author.avatar;
+      pinElement.querySelector("img").alt = slicedData[i].author.title;
+      pinElement.style.left = slicedData[i].location.x + "px";
+      pinElement.style.top = slicedData[i].location.y + "px";
       mapPins.appendChild(pinElement);
     }
   };
+
+  window.successHandler = function (data) {
+    serverData = data;
+    drawPins(data);
+  };
+
+  let updatePins = function () {
+    let sameTypeAccommodation = serverData.filter(function (newPin) {
+      return newPin.offer.type === housingType.value;
+    });
+    removePins();
+    drawPins(sameTypeAccommodation);
+  };
+
+
+  housingType.addEventListener('change', function () {
+    if (housingType.value === 'any') {
+      removePins();
+      drawPins(serverData);
+    } else {
+      updatePins();
+    }
+  });
 
   window.errorHandler = function (errorMessage) {
     let node = document.createElement('div');
