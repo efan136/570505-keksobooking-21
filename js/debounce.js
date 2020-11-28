6ad9a6a6bedec55aac4254b30bc678ad3443f8d7
Andrew <andrei_efremov@inbox.ru>
2020-11-28 23:25:18 +0300

    add comment at debounce

--- js/debounce.js
+++ js/debounce.js
@@ -1,7 +1,7 @@
 'use strict';
 (function () {
 
-  const DEBOUNCE_INTERVAL = 300; // MS
+  const DEBOUNCE_INTERVAL = 300; // MS debounce interval
   let lastTimeout;
   window.debounce = function (cb) {
     if (lastTimeout) {
