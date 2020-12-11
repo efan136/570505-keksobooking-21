8061d087a9ef652d7d31da6251bd85b47dad6fa6
Andrew <andrei_efremov@inbox.ru>
2020-12-12 01:01:18 +0300

    webpack config and creates bundle js

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: [
+    "./js/util.js",
+    "./js/form.js",
+    "./js/upload.js",
+    "./js/load-data.js",
+    "./js/pins.js",
+    "./js/filter.js",
+    "./js/debounce.js",
+    "./js/pinCard.js",
+    "./js/map.js",
+    "./js/main.js",
+    "./js/moveMainPin.js"
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname),
+    iife: true
+  },
+  devtool: false
+};
